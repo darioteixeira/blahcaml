c640780dc78b131b334f3784a4a3528c303c870b
darioteixeira <darioteixeira@yahoo.com>
2008-10-26 21:09:17 +0000

    Renamed mathml2_dtd to mathml2dtd.d

--- dtd_pickler/dtd_pickler.ml
+++ dtd_pickler/dtd_pickler.ml
@@ -10,8 +10,9 @@ let () =
 		let time2 = Unix.gettimeofday () in
 		let marshalled_dtd = Marshal.to_string dtd [Marshal.Closures] in
 		let time3 = Unix.gettimeofday () in
-		let gzip_chan = Gzip.open_out_chan ~level:1 stdout in
-		let () = Gzip.output gzip_chan marshalled_dtd 0 (String.length marshalled_dtd) in
+		(*let gzip_chan = Gzip.open_out_chan ~level:1 stdout in*)
+		(*let () = Gzip.output gzip_chan marshalled_dtd 0 (String.length marshalled_dtd) in*)
+		let () = print_string marshalled_dtd in
 		let time4 = Unix.gettimeofday ()
 		in	Printf.eprintf "DTD pickler: parsing DTD took %5.3f sec.\n" (time2 -. time1);
 			Printf.eprintf "DTD pickler: marshalling DTD took %5.3f sec.\n" (time3 -. time2);
