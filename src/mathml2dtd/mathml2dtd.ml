8a0e19c6965629e378467b54ab801893943c4fb2
darioteixeira <darioteixeira@yahoo.com>
2009-07-26 15:00:52 +0000

    Mathml2dtd now using xstrp4 instead of C stubs

--- src/mathml2dtd/mathml2dtd.ml
+++ src/mathml2dtd/mathml2dtd.ml
@@ -1,77 +1,72 @@
 (********************************************************************************)
-(*	Implementation file for Mathml2dtd.
-
-	Copyright (c) 2008 Dario Teixeira (dario.teixeira@yahoo.com)
-
+(*	Mathml2dtd.ml
+	Copyright (c) 2008-2009 Dario Teixeira (dario.teixeira@yahoo.com)
 	This software is distributed under the terms of the GNU GPL version 2.
 	See LICENSE file for full license text.
 *)
 (********************************************************************************)
 
 open Pxp_types
-open Pxp_document
-open Pxp_yacc
 
 
 (********************************************************************************)
 (*	{2 Private functions}							*)
 (********************************************************************************)
 
-(**	Externally defined functions that return the text of the various
-	individual files constituting the DTD.
+(**	"Pickled" DTD definitions.
 *)
 
-external get_mathml2_dtd:		unit -> string = "get_mathml2_dtd" 
-external get_mathml2_qname_1_mod:	unit -> string = "get_mathml2_qname_1_mod" 
-external get_isoamsa_ent:		unit -> string = "get_isoamsa_ent" 
-external get_isoamsb_ent:		unit -> string = "get_isoamsb_ent" 
-external get_isoamsc_ent:		unit -> string = "get_isoamsc_ent" 
-external get_isoamsn_ent:		unit -> string = "get_isoamsn_ent" 
-external get_isoamso_ent:		unit -> string = "get_isoamso_ent" 
-external get_isoamsr_ent:		unit -> string = "get_isoamsr_ent" 
-external get_isogrk3_ent:		unit -> string = "get_isogrk3_ent" 
-external get_isomfrk_ent:		unit -> string = "get_isomfrk_ent" 
-external get_isomopf_ent:		unit -> string = "get_isomopf_ent" 
-external get_isomscr_ent:		unit -> string = "get_isomscr_ent" 
-external get_isotech_ent:		unit -> string = "get_isotech_ent" 
-external get_isobox_ent:		unit -> string = "get_isobox_ent" 
-external get_isocyr1_ent:		unit -> string = "get_isocyr1_ent" 
-external get_isocyr2_ent:		unit -> string = "get_isocyr2_ent" 
-external get_isodia_ent:		unit -> string = "get_isodia_ent" 
-external get_isolat1_ent:		unit -> string = "get_isolat1_ent" 
-external get_isolat2_ent:		unit -> string = "get_isolat2_ent" 
-external get_isonum_ent:		unit -> string = "get_isonum_ent" 
-external get_isopub_ent:		unit -> string = "get_isopub_ent" 
-external get_mmlextra_ent:		unit -> string = "get_mmlextra_ent" 
-external get_mmlalias_ent:		unit -> string = "get_mmlalias_ent" 
+let mathml2_dtd = include_file "mathml2dtd.d/mathml2.dtd"
+let mathml2_qname_1_mod = include_file "mathml2dtd.d/mathml2-qname-1.mod"
+let isoamsa_ent = include_file "mathml2dtd.d/isoamsa.ent"
+let isoamsb_ent = include_file "mathml2dtd.d/isoamsb.ent"
+let isoamsc_ent = include_file "mathml2dtd.d/isoamsc.ent"
+let isoamsn_ent = include_file "mathml2dtd.d/isoamsn.ent"
+let isoamso_ent = include_file "mathml2dtd.d/isoamso.ent"
+let isoamsr_ent = include_file "mathml2dtd.d/isoamsr.ent"
+let isogrk3_ent = include_file "mathml2dtd.d/isogrk3.ent"
+let isomfrk_ent = include_file "mathml2dtd.d/isomfrk.ent"
+let isomopf_ent = include_file "mathml2dtd.d/isomopf.ent"
+let isomscr_ent = include_file "mathml2dtd.d/isomscr.ent"
+let isotech_ent = include_file "mathml2dtd.d/isotech.ent"
+let isobox_ent = include_file "mathml2dtd.d/isobox.ent"
+let isocyr1_ent = include_file "mathml2dtd.d/isocyr1.ent"
+let isocyr2_ent = include_file "mathml2dtd.d/isocyr2.ent"
+let isodia_ent = include_file "mathml2dtd.d/isodia.ent"
+let isolat1_ent = include_file "mathml2dtd.d/isolat1.ent"
+let isolat2_ent = include_file "mathml2dtd.d/isolat2.ent"
+let isonum_ent = include_file "mathml2dtd.d/isonum.ent"
+let isopub_ent = include_file "mathml2dtd.d/isopub.ent"
+let mmlextra_ent = include_file "mathml2dtd.d/mmlextra.ent"
+let mmlalias_ent = include_file "mathml2dtd.d/mmlalias.ent"
 
 
-(**	Mapping between DTD PUBLIC identifiers and the pickle.
+(**	Mapping between DTD PUBLIC identifiers and the pickles.
 *)
 let catalog =
 	[
-	(Public ("-//W3C//ENTITIES MathML 2.0 Qualified Names 1.0//EN", ""), get_mathml2_qname_1_mod ());
-	(Public ("-//W3C//ENTITIES Added Math Symbols: Arrow Relations for MathML 2.0//EN", ""), get_isoamsa_ent ());
-	(Public ("-//W3C//ENTITIES Added Math Symbols: Binary Operators for MathML 2.0//EN", ""), get_isoamsb_ent ());
-	(Public ("-//W3C//ENTITIES Added Math Symbols: Delimiters for MathML 2.0//EN", ""), get_isoamsc_ent ());
-	(Public ("-//W3C//ENTITIES Added Math Symbols: Negated Relations for MathML 2.0//EN", ""), get_isoamsn_ent ());
-	(Public ("-//W3C//ENTITIES Added Math Symbols: Ordinary for MathML 2.0//EN", ""), get_isoamso_ent ());
-	(Public ("-//W3C//ENTITIES Added Math Symbols: Relations for MathML 2.0//EN", ""), get_isoamsr_ent ());
-	(Public ("-//W3C//ENTITIES Greek Symbols for MathML 2.0//EN", ""), get_isogrk3_ent ());
-	(Public ("-//W3C//ENTITIES Math Alphabets: Fraktur for MathML 2.0//EN", ""), get_isomfrk_ent ());
-	(Public ("-//W3C//ENTITIES Math Alphabets: Open Face for MathML 2.0//EN", ""), get_isomopf_ent ());
-	(Public ("-//W3C//ENTITIES Math Alphabets: Script for MathML 2.0//EN", ""), get_isomscr_ent ());
-	(Public ("-//W3C//ENTITIES General Technical for MathML 2.0//EN", ""), get_isotech_ent ());
-	(Public ("-//W3C//ENTITIES Box and Line Drawing for MathML 2.0//EN", ""), get_isobox_ent ());
-	(Public ("-//W3C//ENTITIES Russian Cyrillic for MathML 2.0//EN", ""), get_isocyr1_ent ());
-	(Public ("-//W3C//ENTITIES Non-Russian Cyrillic for MathML 2.0//EN", ""), get_isocyr2_ent ());
-	(Public ("-//W3C//ENTITIES Diacritical Marks for MathML 2.0//EN", ""), get_isodia_ent ());
-	(Public ("-//W3C//ENTITIES Added Latin 1 for MathML 2.0//EN", ""), get_isolat1_ent ());
-	(Public ("-//W3C//ENTITIES Added Latin 2 for MathML 2.0//EN", ""), get_isolat2_ent ());
-	(Public ("-//W3C//ENTITIES Numeric and Special Graphic for MathML 2.0//EN", ""), get_isonum_ent ());
-	(Public ("-//W3C//ENTITIES Publishing for MathML 2.0//EN", ""), get_isopub_ent ());
-	(Public ("-//W3C//ENTITIES Extra for MathML 2.0//EN", ""), get_mmlextra_ent ());
-	(Public ("-//W3C//ENTITIES Aiases for MathML 2.0//EN", ""), get_mmlalias_ent ());
+	(Public ("-//W3C//ENTITIES MathML 2.0 Qualified Names 1.0//EN", ""), mathml2_qname_1_mod);
+	(Public ("-//W3C//ENTITIES Added Math Symbols: Arrow Relations for MathML 2.0//EN", ""), isoamsa_ent);
+	(Public ("-//W3C//ENTITIES Added Math Symbols: Binary Operators for MathML 2.0//EN", ""), isoamsb_ent);
+	(Public ("-//W3C//ENTITIES Added Math Symbols: Delimiters for MathML 2.0//EN", ""), isoamsc_ent);
+	(Public ("-//W3C//ENTITIES Added Math Symbols: Negated Relations for MathML 2.0//EN", ""), isoamsn_ent);
+	(Public ("-//W3C//ENTITIES Added Math Symbols: Ordinary for MathML 2.0//EN", ""), isoamso_ent);
+	(Public ("-//W3C//ENTITIES Added Math Symbols: Relations for MathML 2.0//EN", ""), isoamsr_ent);
+	(Public ("-//W3C//ENTITIES Greek Symbols for MathML 2.0//EN", ""), isogrk3_ent);
+	(Public ("-//W3C//ENTITIES Math Alphabets: Fraktur for MathML 2.0//EN", ""), isomfrk_ent);
+	(Public ("-//W3C//ENTITIES Math Alphabets: Open Face for MathML 2.0//EN", ""), isomopf_ent);
+	(Public ("-//W3C//ENTITIES Math Alphabets: Script for MathML 2.0//EN", ""), isomscr_ent);
+	(Public ("-//W3C//ENTITIES General Technical for MathML 2.0//EN", ""), isotech_ent);
+	(Public ("-//W3C//ENTITIES Box and Line Drawing for MathML 2.0//EN", ""), isobox_ent);
+	(Public ("-//W3C//ENTITIES Russian Cyrillic for MathML 2.0//EN", ""), isocyr1_ent);
+	(Public ("-//W3C//ENTITIES Non-Russian Cyrillic for MathML 2.0//EN", ""), isocyr2_ent);
+	(Public ("-//W3C//ENTITIES Diacritical Marks for MathML 2.0//EN", ""), isodia_ent);
+	(Public ("-//W3C//ENTITIES Added Latin 1 for MathML 2.0//EN", ""), isolat1_ent);
+	(Public ("-//W3C//ENTITIES Added Latin 2 for MathML 2.0//EN", ""), isolat2_ent);
+	(Public ("-//W3C//ENTITIES Numeric and Special Graphic for MathML 2.0//EN", ""), isonum_ent);
+	(Public ("-//W3C//ENTITIES Publishing for MathML 2.0//EN", ""), isopub_ent);
+	(Public ("-//W3C//ENTITIES Extra for MathML 2.0//EN", ""), mmlextra_ent);
+	(Public ("-//W3C//ENTITIES Aiases for MathML 2.0//EN", ""), mmlalias_ent);
 	]
 
 
@@ -86,8 +81,8 @@ let get_dtd () =
 	try
 		let resolver = new Pxp_reader.lookup_id_as_string catalog in
 		let config = {default_config with encoding = `Enc_utf8} in
-		let dtd = parse_dtd_entity config (from_string ~alt:[resolver] (get_mathml2_dtd ()))
-		in dtd
+		let source = Pxp_types.from_string ~alt:[resolver] mathml2_dtd
+		in Pxp_dtd_parser.parse_dtd_entity config source
 	with
 		exc ->
 			print_endline (Pxp_types.string_of_exn exc);
--- src/mathml2dtd/mathml2dtd.mli
+++ src/mathml2dtd/mathml2dtd.mli
@@ -1,8 +1,6 @@
 (********************************************************************************)
-(*	Interface file for Mathml2dtd.
-
-	Copyright (c) 2008 Dario Teixeira (dario.teixeira@yahoo.com)
-
+(*	Mathml2dtd.mli
+	Copyright (c) 2008-2009 Dario Teixeira (dario.teixeira@yahoo.com)
 	This software is distributed under the terms of the GNU GPL version 2.
 	See LICENSE file for full license text.
 *)
