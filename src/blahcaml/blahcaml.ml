d357c4882a4fb34dfe6061972da326f478e53eb1
darioteixeira <darioteixeira@yahoo.com>
2013-07-20 13:51:23 +0000

    Updated copyright dates

--- src/blahcaml/blahcaml.ml
+++ src/blahcaml/blahcaml.ml
@@ -1,6 +1,6 @@
 (********************************************************************************)
 (*	Blahcaml.ml
-	Copyright (c) 2008-2010 Dario Teixeira (dario.teixeira@yahoo.com)
+	Copyright (c) 2008-2013 Dario Teixeira (dario.teixeira@yahoo.com)
 	This software is distributed under the terms of the GNU GPL version 2.
 	See LICENSE file for full license text.
 *)
--- src/messages/Messages.h
+++ src/messages/Messages.h
@@ -1,7 +1,7 @@
 /********************************************************************************/
 /*	Interface file for Messages.
 
-	Copyright (c) 2008 Dario Teixeira (dario.teixeira@yahoo.com)
+	Copyright (c) 2008-2013 Dario Teixeira (dario.teixeira@yahoo.com)
 
 	This software is distributed under the terms of the GNU GPL version 2.
 	See LICENSE file for full license text.
